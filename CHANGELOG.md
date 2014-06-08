68599a3db1348796c3d0fb428d2a8e0c8e265203
Tom Hulihan <hulihan.tom159@gmail.com>
2014-06-08 13:36:08 -0400

    Fix warnings generated by hackage

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# 0.1.0.0
+
+* First public release
+* Add the `MonadRequest` type class
+* Add the `RequestT` monad
+* Initial documentation
--- src/Control/Monad/Request.hs
+++ src/Control/Monad/Request.hs
@@ -1,4 +1,23 @@
--- | Request monads.
+{- |
+Module      :  Control.Monad.Request.Class
+Copyright   :  (c) Tom Hulihan <hulihan.tom159@gmail.com> 2014,
+License     :  MIT
+
+Maintainer  :  hulihan.tom159@gmail.com
+Stability   :  experimental
+Portability :  non-portable (multi-parameter type classes)
+
+[Computation type:] Compuations that send requests and handle responses
+
+[Binding strategy:] Response callbacks are composed with the binding function
+
+[Useful for:] Implementation-agnostic requests (i.e. making real requests versus
+mocking), adding middlewares.
+
+[Example type:] @'Control.Monad.Request.Lazy.Request' String String a@
+
+The Request monad
+-}
 module Control.Monad.Request ( module Control.Monad.Request.Lazy ) where
 
 import Control.Monad.Request.Lazy
